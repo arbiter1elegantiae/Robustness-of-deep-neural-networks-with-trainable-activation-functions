765776d3451ec6046fc828d6c9702065be9a0533
federico <peconi.federico@gmail.com>
2020-05-14 15:50:45 +0000

    added kafs, relu/kwta/kaf model and scheleton for the attack

--- kwta.py
+++ kwta.py
@@ -11,9 +11,9 @@ class Kwta(Layer):
     Parameters
     ----------
    """
-    def __init__(self, ratio=0.2, conv = False, data_format="channels_last"):
+    def __init__(self, ratio=0.2, conv = False, data_format="channels_last", **kwargs):
 
-        super(Kwta, self).__init__()
+        super(Kwta, self).__init__(**kwargs)
 
         self.conv = conv
         self.ratio = ratio
@@ -21,39 +21,52 @@ class Kwta(Layer):
         
 
     def build(self, input_shape):
+        
+        # dim = C*H*W when conv=True, #units otherwise
+        self.dim = tf.math.reduce_prod(input_shape[1:]).numpy() 
 
-        if not self.conv:
-            self.k = int(math.ceil(self.ratio*input_shape[1]))
-
-        else:
-            dim = tf.math.reduce_prod(input_shape[1:]).numpy()
-            self.k = int(math.ceil(self.ratio*dim))            
-
-        self.shape = input_shape
 
 
     def call(self, inputs):
+        
+        # In case of incremental learning we want to update k 
+        k = int(math.ceil(self.ratio*self.dim))
 
+        # Store input shape to the layer
+        shape = tf.shape(inputs)
+        
         if self.conv:
-
+            
             if self.data_format == "channels_last":
-                # Transpose to have channel dimension as the first dimension
-                outputs = tf.transpose(inputs, perm = [0, 3, 1, 2])
-            else:
-                outputs = inputs
+                # Transpose to have channel_first format
+                inputs = tf.transpose(inputs, perm = [0, 3, 1, 2])
 
             # Flatten the tensor
-            dim = tf.math.reduce_prod(self.shape[1:]) # dim = C*H*W
-            outputs = tf.reshape(outputs, [-1, dim])
-            # Get the largest K-th value from each flattened tensor in the batch
-            kths_largest = tf.math.top_k(outputs, self.k, sorted=True)[0][:,-1]
-            
-            # Set to 0 all the values but the K largests per batch
-            outputs = tf.where(tf.math.less(outputs, tf.expand_dims(kths_largest, -1)), 0.0, outputs)
+            inputs = tf.reshape(inputs, [-1, self.dim])
+        
+        # Get the largest K-th value from each flattened tensor in the batch
+        kths_largest = tf.math.top_k(inputs, k, sorted=True)[0][:,-1]
+        
+        # Set to 0 all the values but the K largests per batch
+        outputs = tf.where(tf.math.less(inputs, tf.expand_dims(kths_largest, -1)), 0.0, inputs)
 
+        if self.conv:
             # Transform back to the original shape
-            outputs = tf.reshape(outputs, shape = tf.shape(inputs))
-        else:
-            outputs = tf.math.top_k(inputs, self.k, sorted=True)[0]
+            outputs = tf.reshape(outputs, shape)
         
         return outputs
+
+    
+    def get_config(self):
+        
+        my_config = {
+            
+            'conv' : self.conv,
+            'ratio' : self.ratio,
+            'data_format' : self.data_format,
+            'dim' : self.dim
+        }
+
+        base_config = super(Kwta, self).get_config()
+        return dict(list(base_config.items()) + list(my_config.items()))
+
